2ea59eda6313e277fb9bd30c783a5a6532b3e3bf
anhnguyen <tuananh871995@gmail.com>
2022-04-21 11:44:45 +0700

    Implement common text component

---
+++ src/components/inputs/textInput.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './text-input.css';
+
+const TextInput = props => {
+  const { type, readonly, actionIcon, defaultValue } = props;
+  return <div className={`r-text-input-container ${readonly && 'readonly'}`}>
+    {
+      readonly ?
+        <div className="r-text-input-readonly">
+          {defaultValue}
+        </div> :
+        <input
+          type={type}
+          className="r-text-input"
+        />
+    }
+    {actionIcon}
+  </div>;
+};
+TextInput.propTypes = {
+  type: PropTypes.string,
+  readonly: PropTypes.bool,
+  actionIcon: PropTypes.node,
+  defaultValue: PropTypes.string,
+};
+TextInput.defaultProps = {
+  type: 'text',
+  readonly: false,
+  defaultValue: '',
+};
+
+export default TextInput;
--- src/containers/unlock-wallet/index.js
+++ src/containers/unlock-wallet/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import Logo from 'static/icons/ronin-logo-blue.png';
 import EyeIcon from 'static/icons/open-eye-icon.png';
 import Button from 'components/buttons/button';
+import TextInput from 'components/inputs/textInput';
 
 import './unlock-wallet.css';
 
 const UnlockWallet = () => {
+  const ActionComponent = <img className="uw-eye-icon" alt="show" src={EyeIcon} />;
   return <div className="uw-container">
     <div className="uw-header">
       <img className="uw-logo" alt="ronin" src={Logo} />
@@ -14,10 +16,7 @@ const UnlockWallet = () => {
     </div>
     <div className="uw-form">
       <div className="uw-form-title">{'Enter password'}</div>
-      <div className="r-input-container">
-        <input type="password" className="r-input" />
-        <img className="r-input-icon" alt="show" src={EyeIcon} />
-      </div>
+      <TextInput type="password" actionIcon={ActionComponent} />
       <div className="uw-control">
         <Button onClick={() => {}} text={'Unlock'} />
       </div>
